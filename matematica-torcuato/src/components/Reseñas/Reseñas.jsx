3bcc02489bdf1de087c0ea64c0f325abd5905f64
TissoniTomas <tissonitomas5@gmail.com>
2024-08-05 10:47:17 -0300

    reseñas

--- matematica-torcuato/src/components/Reseñas/Reseñas.jsx
+++ matematica-torcuato/src/components/Reseñas/Reseñas.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Reseñas = () => {
   return (
     <div className='mb-20'>
-        <h3 className="text-2xl font-Montserrat text-center lg:text-6xl text-black">Nuestros alumnos nos certifican</h3>
+        <h3 className="text-2xl font-Montserrat text-center lg:text-6xl text-black">Nuestros alumnos certifican nuestra trayectoria.</h3>
     </div>
   )
 }
